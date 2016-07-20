23d10d0653233e7158670f3da7185cff0a1563d5
Juntai Park <juntai81@gmail.com>
2016-07-21 01:23:25 +0900

    add sample data

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 
 app.use('/img', express.static(__dirname + '/img'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/test', function(req, res) {
